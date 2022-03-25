ea95f4b9170009df66d2217cd3d2af48e4f265cf
Soulmaers <certiss@ya.ru>
2022-03-25 17:29:31 +0300

    Логика+время

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 
 
-//Объект 1, забираем id
-
-const lft01 = document.getElementById('left_front_tires_01');
-const rft01 = document.getElementById('right_front_tires_01');
-const lrt01 = document.getElementById('left_rear_tires_01');
-const rrt01 = document.getElementById('right_rear_tires_01');
+//забираем объект
+let alls = document.getElementsByClassName('tires');
 const stat1 = document.getElementById('obj1');
+let time1 = document.getElementById('tObj1');
+//Объект 1, 
+
 // рандомные значения
 function math1() {
     randomlft01 = Math.floor(Math.random() * 10);
@@ -17,66 +16,63 @@ function math1() {
 // логика выбора
 const func1 = () => {
     math1();
+    time1.textContent = getNowtime();
     if (randomlrt01 >= 4) {
-        lrt01.style.background = 'green';
+        alls[2].style.background = 'green';
         stat1.style.backgroundImage = "url(image/gal.png)";
+
     }
     if (randomrrt01 >= 4) {
-        rrt01.style.background = 'green';
+        alls[3].style.background = 'green';
         stat1.style.backgroundImage = "url(image/gal.png)";
     }
     if (randomlft01 >= 4) {
-        lft01.style.background = 'green';
+        alls[0].style.background = 'green';
         stat1.style.backgroundImage = "url(image/gal.png)";
     }
     if (randomrft01 >= 4) {
-        rft01.style.background = 'green';
+        alls[1].style.background = 'green';
         stat1.style.backgroundImage = "url(image/gal.png)";
     }
     if (randomlrt01 >= 2 && randomlrt01 < 4) {
-        lrt01.style.background = 'yellow';
+        alls[2].style.background = 'yellow';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrrt01 >= 2 && randomrrt01 < 4) {
-        rrt01.style.background = 'yellow';
+        alls[3].style.background = 'yellow';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlrt01 < 2) {
-        rrt01.style.background = 'red';
+        alls[2].style.background = 'red';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrrt01 < 2) {
-        rft01.style.background = 'red';
+        alls[3].style.background = 'red';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlft01 >= 2 && randomlft01 < 4) {
-        lft01.style.background = 'yellow';
+        alls[0].style.background = 'yellow';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrft01 >= 2 && randomrft01 < 4) {
-        rft01.style.background = 'yellow';
+        alls[1].style.background = 'yellow';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlft01 < 2) {
-        lft01.style.background = 'red';
+        alls[0].style.background = 'red';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrft01 < 2) {
-        rft01.style.background = 'red';
+        alls[1].style.background = 'red';
         stat1.style.backgroundImage = "url(image/er.png)";
     }
-
 }
 func1();
 setInterval(func1, 1000);
 
-
-//Объект 2, забираем id
-const lft02 = document.getElementById('left_front_tires_02');
-const rft02 = document.getElementById('right_front_tires_02');
-const lrt02 = document.getElementById('left_rear_tires_02');
-const rrt02 = document.getElementById('right_rear_tires_02');
+//Объект 2
 const stat2 = document.getElementById('obj2');
+let time2 = document.getElementById('tObj2');
 // рандомные значения
 function math2() {
     randomlft02 = Math.floor(Math.random() * 10);
@@ -87,92 +83,83 @@ function math2() {
 // логика выбора
 const func2 = () => {
     math2();
+    time2.textContent = getNowtime();
     if (randomlrt02 >= 4) {
-        lrt02.style.background = 'green';
+        alls[6].style.background = 'green';
         stat2.style.backgroundImage = "url(image/gal.png)";
-        console.log(randomlrt02);
     }
     if (randomrrt02 >= 4) {
-        rrt02.style.background = 'green';
+        alls[7].style.background = 'green';
         stat2.style.backgroundImage = "url(image/gal.png)";
-        console.log(randomrrt02);
     }
     if (randomlft02 >= 4) {
-        lft02.style.background = 'green';
+        alls[4].style.background = 'green';
         stat2.style.backgroundImage = "url(image/gal.png)";
-        console.log(randomlft02);
     }
     if (randomrft02 >= 4) {
-        rft02.style.background = 'green';
+        alls[5].style.background = 'green';
         stat2.style.backgroundImage = "url(image/gal.png)";
-        console.log(randomrft02);
     }
     if (randomlrt02 >= 2 && randomlrt02 < 4) {
-        lrt02.style.background = 'yellow';
+        alls[6].style.background = 'yellow';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrrt02 >= 2 && randomrrt02 < 4) {
-        rrt02.style.background = 'yellow';
+        alls[7].style.background = 'yellow';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlrt02 < 2) {
-        lrt02.style.background = 'red';
+        alls[6].style.background = 'red';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrrt02 < 2) {
-        rrt02.style.background = 'red';
+        alls[7].style.background = 'red';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlft02 >= 2 && randomlft02 < 4) {
-        lft02.style.background = 'yellow';
+        alls[4].style.background = 'yellow';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrft02 >= 2 && randomrft02 < 4) {
-        rft02.style.background = 'yellow';
+        alls[5].style.background = 'yellow';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomlft02 < 2) {
-        lft02.style.background = 'red';
+        alls[4].style.background = 'red';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
     if (randomrft02 < 2) {
-        rft02.style.background = 'red';
+        alls[5].style.background = 'red';
         stat2.style.backgroundImage = "url(image/er.png)";
     }
 
 }
 func2();
-setInterval(func2, 1000);
+setInterval(func2, 2000);
 
 
 
 
-
-
-
-
-/*const tires = document.getElementById('left_front_tires_01');
-const stat = document.getElementById('obj1');
-const func = () => {
-    countTires = (Math.floor(Math.random() * 10));
-    if (countTires <= 10 && countTires >= 7) {
-        tires.style.background = 'green';
-        stat.style.backgroundImage = "url(image/gal.png)";
+//текущее время
+function getNowtime() {
+    let now = new Date();
+    let hours = now.getHours();
+    if (now.getHours() < 10) {
+        hours = "0" + hours;
     }
-    if (countTires < 7 && countTires >= 4) {
-        tires.style.background = 'yellow';
-        stat.style.backgroundImage = "url(image/er.png)";
+    let minutes = now.getMinutes();
+    if (now.getMinutes() < 10) {
+        minutes = "0" + minutes;
     }
-    if (countTires < 4) {
-        tires.style.background = 'red';
-        stat.style.backgroundImage = "url(image/er.png)";
+    let seconds = now.getSeconds();
+    if (now.getSeconds() < 10) {
+        seconds = "0" + minutes;
     }
+    let nowTime = (`${hours}:${minutes}:${seconds}`);
+    return nowTime;
 }
-func();
-setInterval(func, 1000);*/
 
 
-//let valueTires = parseInt(prompt('Введите значение давления в шине'));
 
 
 
