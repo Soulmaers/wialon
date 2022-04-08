235ce02a82dc5f895010813c06d3a70b7ffcb909
Soulmaers <certiss@ya.ru>
2022-04-08 08:23:33 +0300

    дашбоард

--- js/dash.js
+++ js/dash.js
@@ -1,105 +1,42 @@
-var Piechart = function (options) {
-    this.options = options;
-    this.canvas = options.canvas;
-    this.ctx = this.canvas.getContext("2d");
-    this.colors = options.colors;
-
-    this.draw = function () {
-        var total_value = 0;
-        var color_index = 0;
-        for (var categ in this.options.data) {
-            var val = this.options.data[categ];
-            total_value += val;
-        }
-
-        var start_angle = 0;
-        for (categ in this.options.data) {
-            val = this.options.data[categ];
-            var slice_angle = 2 * Math.PI * val / total_value;
-
-            drawPieSlice(
-                this.ctx,
-                this.canvas.width / 2,
-                this.canvas.height / 2,
-                Math.min(this.canvas.width / 2, this.canvas.height / 2),
-                start_angle,
-                start_angle + slice_angle,
-                this.colors[color_index % this.colors.length]
-            );
-
-            start_angle += slice_angle;
-            color_index++;
-        }
-        if (this.options.doughnutHoleSize) {
-            drawPieSlice(
-                this.ctx,
-                this.canvas.width / 2,
-                this.canvas.height / 2,
-                this.options.doughnutHoleSize * Math.min(this.canvas.width / 2, this.canvas.height / 2),
-                0,
-                2 * Math.PI,
-                "#ff0000"
-            );
-        }
-        start_angle = 0;
-        for (categ in this.options.data) {
-            val = this.options.data[categ];
-            slice_angle = 2 * Math.PI * val / total_value;
-            var pieRadius = Math.min(this.canvas.width / 2, this.canvas.height / 2);
-            var labelX = this.canvas.width / 2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle / 2);
-            var labelY = this.canvas.height / 2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle / 2);
-
-            if (this.options.doughnutHoleSize) {
-                var offset = (pieRadius * this.options.doughnutHoleSize) / 2;
-                labelX = this.canvas.width / 2 + (offset + pieRadius / 2) * Math.cos(start_angle + slice_angle / 2);
-                labelY = this.canvas.height / 2 + (offset + pieRadius / 2) * Math.sin(start_angle + slice_angle / 2);
-            }
-
-            var labelText = Math.round(100 * val / total_value);
-            this.ctx.fillStyle = "white";
-            this.ctx.font = "bold 20px Arial";
-            this.ctx.fillText(labelText + "%", labelX, labelY);
-            start_angle += slice_angle;
-        }
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Большое отклонение',
+            'Незначительные отклонения',
+            'В Норме'
+        ],
+        datasets: [{
+            label: 'Дашбоард',
+            data: [10, 20, 70],
+            backgroundColor: [
+                '#e03636',
+                '#9ba805',
+                '#3eb051'
+            ],
+            hoverOffset: 4
+        }]
     }
-    if (this.options.legend) {
-        color_index = 0;
-        var legendHTML = "";
-        for (categ in this.options.data) {
-            legendHTML += "<div><span style='display:inline-block;width:20px;background-color:" + this.colors[color_index++] + ";'>&nbsp;</span> " + categ + "</div>";
-        }
-        this.options.legend.innerHTML = legendHTML;
+})
+
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const chart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Работающие датчики',
+            'Нет данных от датчиков'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [90, 10],
+            backgroundColor: [
+                '#3eb051',
+                'gray'
+            ],
+            hoverOffset: 4
+        }]
     }
-}
-
-
-var myPiechart = new Piechart(
-    {
-        canvas: myCanvas,
-        data: myVinyls,
-        colors: ["#fde23e", "#f16e23", "#57d9ff", "#937e88"]
-    }
-);
-myPiechart.draw();
-
-var myDougnutChart = new Piechart(
-    {
-        canvas: myCanvas,
-        data: myVinyls,
-        colors: ["#fde23e", "#f16e23", "#57d9ff", "#937e88"],
-        doughnutHoleSize: 0.5
-    }
-);
-myDougnutChart.draw();
-
-var myLegend = document.getElementById("myLegend");
-
-var myDougnutChart = new Piechart(
-    {
-        canvas: myCanvas,
-        data: myVinyls,
-        colors: ["#fde23e", "#f16e23", "#57d9ff", "#937e88"],
-        legend: myLegend
-    }
-);
-myDougnutChart.draw();
+})
